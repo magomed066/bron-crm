7f68cf84a939a12b709147837f474818203ef95f
Magomed Mamergov <m.mamergovv@yandex.ru>
2024-12-31 17:21:38 +0300

    pagination for all orders and delete order

--- backend/app/src/routes/orders/index.js
+++ backend/app/src/routes/orders/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import checkAuth from '../../lib/helpers/check-auth.js'
 import {
 	createOrder,
+	deleteOrder,
 	getAllOrder,
 	getOrderById,
 	updatedOrder,
@@ -13,5 +14,6 @@ router.post('/create', checkAuth, createOrder)
 router.get('/all', checkAuth, getAllOrder)
 router.get('/:id', checkAuth, getOrderById)
 router.post('/update', checkAuth, updatedOrder)
+router.delete('/delete/:id', checkAuth, deleteOrder)
 
 export default router
